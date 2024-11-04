70eac2caaf0034dd35195500f4df1a6a8daa1bb4
ferrifirmansyah <ferrifirmansyah54@gmail.com>
2024-11-04 08:44:34 +0700

    ubah class Motorcycle menggunakan fungsi java

--- ISP/Motorcycle.java
+++ ISP/Motorcycle.java
@@ -1,5 +1,13 @@
-class Motorcycle : VehicleInterface {
-    override fun drive() {}
-    override fun stop() {}
-    override fun refuel() {}
+class Motorcycle implements VehicleInterface {
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public void refuel() {
+    }
 }
