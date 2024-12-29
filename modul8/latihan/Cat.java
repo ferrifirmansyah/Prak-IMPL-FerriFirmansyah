5eb611f75eb7059fe194d2fe6e6133cf659f0b87
ferrifirmansyah <ferrifirmansyah54@gmail.com>
2024-12-29 19:42:35 +0700

    implementasi class kucing

---
+++ modul8/latihan/Cat.java
@@ -0,0 +1,11 @@
+class Cat{ 
+    private String name; 
+    private int age; 
+    public void run(){ 
+        System.out.print(“I am running”); 
+    } 
+    public void mew(){ 
+        System.out.print(“miawww...”); 
+    } 
+    //dan method lainnya... 
+} 
